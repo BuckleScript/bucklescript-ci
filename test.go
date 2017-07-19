60588b3f62799a989ee818ba66b661681b20783b
Hongbo Zhang <bobzhang1988@gmail.com>
2017-07-19 16:36:51 +0800

    add more tests

--- test.go
+++ test.go
@@ -6,6 +6,7 @@ import (
 		"os/exec"
 		"fmt"
 		"log"
+	"os"
 		)
 
 func checkError(err error){
@@ -13,10 +14,33 @@ func checkError(err error){
 		log.Fatalf("Error: %s", err.Error())
 	}
 }
+
+func testTheme(theme string){
+	os.RemoveAll(theme)
+	cmd := exec.Command("bsb", "-theme",theme,"-init",theme)
+	output, err:= cmd.CombinedOutput()
+	
+	fmt.Println(string(output))
+	checkError(err)
+	
+	fmt.Println("Started to build ")
+	cmd2 := exec.Command("npm", "run", "build")		
+	cmd2.Dir = theme
+	output2, err:= cmd2.CombinedOutput()
+	fmt.Println(string(output2))
+	checkError(err)		
+	os.RemoveAll(theme)
+}
 func main(){
 
 	output, err := exec.Command("which", "bsb").Output()
 	checkError(err)	
-	fmt.Println(
-		string(output))
+	fmt.Println("bsb", "is", string(output))
+	
+	// testTheme("basic")
+	// testTheme("generator")
+	for _, theme := range []string{"basic", "basic-reason","generator","minimal"} {
+		fmt.Println("Test theme", theme)
+		testTheme(theme)
+	}
 }		
